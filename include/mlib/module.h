91465eaaac39ede501687c0b5fc5af02caa65a62
Alex Waterman <imNotListening@gmail.com>
2013-01-27 22:45:18 -0800

    Add the first part of loadable module support
    This contains a simple test module for development purposes. In
    the future modules will be used to implement key functionality
    like play back and extra library management features.
    
    Signed-off-by: Alex Waterman <imNotListening@gmail.com>

---
+++ include/mlib/module.h
@@ -0,0 +1,58 @@
+/* (C) Copyright 2013
+ * Alex Waterman <imNotListening@gmail.com>
+ *
+ * mlib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mlib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mlib.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Definitions for making a module and the core module code itself.
+ */
+
+#ifndef _MLIB_MODULE_H_
+#define _MLIB_MODULE_H_
+
+/**
+ * A structure to describe a module. This must be present in each module. See
+ * the macro MLIB_MODULE().
+ */
+struct mlib_module {
+	const char		*name;
+	int			(*init)(void);
+	int			(*fini)(void);
+	struct list_head	 list_node;
+};
+
+/*
+ * Module functions for everyone to use.
+ */
+
+/*
+ * Module functions for mlib core to use.
+ */
+
+
+/**
+ * This macro should be called once and only once for each module to be loaded
+ * by mlib.
+ *
+ * @name	Name of the module. Should be a 'char *'.
+ * @init	Init function. Gets called right after being loaded.
+ * @fini	Function called to cleanup any module resources on closuer.
+ */
+#define MLIB_MODULE(NAME, INIT, FINI)			\
+	const struct mlib_module __mlib_module_desc = {	\
+		.name = NAME,				\
+		.init = INIT,				\
+		.fini = FINI,				\
+	};
+
+#endif
--- modules/test_mod/test_mod.c
+++ modules/test_mod/test_mod.c
@@ -14,26 +14,23 @@
  * You should have received a copy of the GNU General Public License
  * along with mlib.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Load modules.
+ * Simple test module.
  */
 
-#include <dlfcn.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 
 #include <mlib/mlib.h>
-#include <mlib/module.h>
 
-static LIST_HEAD(mlib_modules);
-
-/**
- * Load the module in the passed string. A 0 is returned if successful, other
- * wise < 0 will be returned.
- *
- * @path	Path to the module. Will be resolved based on shared library
- * 		loadign rules.
- */
-int mlib_load_module(const char *path)
+int test_mod_init(void)
 {
+	mlib_printf("Yo, I'm a test module.\n");
 	return 0;
 }
+
+int test_mod_fini(void)
+{
+	mlib_printf("Good bye from a test module.\n");
+	return 0;
+}
+
+MLIB_MODULE("test-mod", test_mod_init, test_mod_fini);
